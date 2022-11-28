62a4facb1f42691f33b173c425e231d8cbb7bd50
FarisTF <faris.tristan@gmail.com>
2022-11-28 17:29:42 +0700

    nambah create

--- send_email.js
+++ send_email.js
@@ -1,24 +1,72 @@
-var nodemailer = require("nodemailer");
-var database = require("./database.json");
+const dataPath = "./database.json"; // path to our JSON file
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
-console.log(database["data"].length);
+// util functions
+const saveTodolistData = (data) => {
+    const stringifyData = JSON.stringify(data);
+    fs.writeFileSync(dataPath, stringifyData);
+};
+const getTodolistData = () => {
+    const jsonData = fs.readFileSync(dataPath);
+    return JSON.parse(jsonData);
+};
+
+const port = 4242;
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Create - Tambah todolist
+app.post("/todolist", (req, res) => {
+    var existTodolist = getTodolistData();
+    const newTodolistId = Math.floor(100000 + Math.random() * 900000);
+
+    existTodolist[newTodolistId] = req.body;
+
+    console.log(existTodolist);
+    saveTodolistData(existTodolist);
+    res.send({ success: true, msg: "Todolist added successfully" });
+});
+
+// Read - Ambil semua todolist
+app.get("/todolist", (req, res) => {
+    const todolist = getTodolistData();
+    res.send(todolist);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
 
 is_not_done = false;
 kegiatan_not_done = "";
+database = getTodolistData();
 
-for (var i = 0; i < database["data"].length; i++) {
-    if (database["data"][i]["is_done"] == false) {
+for (let x in database) {
+    console.log(x + ": ");
+    console.log(database[x]);
+    console.log("\n");
+}
+
+for (let x in database) {
+    if (database[x]["is_done"] == false) {
         is_not_done = true;
-        kegiatan_not_done += database["data"][i]["activity"];
+        kegiatan_not_done += database[x]["activity"];
         kegiatan_not_done += "\n";
-        kegiatan_not_done += database["data"][i]["course_name"];
+        kegiatan_not_done += database[x]["course_name"];
         kegiatan_not_done += "\n";
-        kegiatan_not_done += database["data"][i]["deadline"];
+        kegiatan_not_done += database[x]["deadline"];
         kegiatan_not_done += "\n\n";
     }
 }
 
+var nodemailer = require("nodemailer");
+
 console.log(kegiatan_not_done);
+
+console.log(Object.keys(database).length);
 
 // var transporter = nodemailer.createTransport({
 //     service: "gmail",
@@ -41,13 +89,3 @@ console.log(kegiatan_not_done);
 //         console.log("Email sent: " + info.response);
 //     });
 // }
-
-const express = require("express");
-const port = 4242;
-const app = express();
-
-app.get("/todolist", async function (req, res, next) {
-    res.json(database);
-});
-
-app.listen(port, () => console.log(`Listening on port ${port}!`));
