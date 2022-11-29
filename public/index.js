5a88b9286f24eacbdc7a6326274d1ce6ede77b1a
ammarraihn <ammarraihan00@gmail.com>
2022-11-29 20:17:20 +0700

    fetch json to html done

--- public/index.js
+++ public/index.js
@@ -1,22 +1,79 @@
-fetch('../database.json')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        appendData(data);
-    })
-    .catch(function (err) {
-        console.log('error: ' + err);
-    });
+const completeButton = document.querySelector(".complete-btn");
+
+var generateBtn = document.getElementById("fetchbutton");
+generateBtn.addEventListener("click", fetchdata);
+
+function fetchdata() {
+    fetch("http://localhost:4242/todolist")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            appendData(data);
+        })
+        .catch(function (err) {
+            console.log("error: " + err);
+        });
 
 
-function appendData(data) {
-    var mainContainer = document.getElementById("data");
-    for (var i = 0; i < data.length; i++) {
-        var todo = document.createElement("div");
-        todo.innerHTML = data.result[i].name;
-        todo.innerHTML = data.result[i].course_name;
-        todo.innerHTML = data.result[i].deadline;
-        mainContainer.appendChild(div);
+    function appendData(data) {
+        var mainContainer = document.getElementById("data");
+        if (mainContainer.innerHTML = "");
+            for (let x in data) {
+                if (data[x].is_done == false) {
+                    const todoDiv = document.createElement("div");
+                    todoDiv.classList.add("todo");
+
+                    const course = document.createElement("div");
+                    course.textContent = data[x]["course_name"];
+
+                    const activity = document.createElement("div");
+                    activity.textContent = data[x]["activity"];
+
+                    const deadline = document.createElement("div");
+                    deadline.textContent = data[x]["deadline"];
+
+                    const completeButton = document.createElement("button");
+                    completeButton.textContent = 'V';
+
+                    // const breakline = document.createElement("br");
+                    // breakline.textContent = " ";
+
+                    todoDiv.appendChild(course);
+                    todoDiv.appendChild(activity);
+                    todoDiv.appendChild(deadline);
+                    todoDiv.appendChild(completeButton);
+
+
+                    mainContainer.appendChild(todoDiv);
+                }
+        }
     }
+}
+
+
+function updateTodo() {
+    var todo = document.getElementById("todo").value;
+    var course = document.getElementById("course").value;
+    var deadline = document.getElementById("deadline").value;
+    var data = {
+        "activity": todo,
+        "course_name": course,
+        "deadline": deadline,
+        "is_done": true,
+    }
+    fetch('http://localhost:4242/todolist/:id', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 }
--- send_email.js
+++ send_email.js
@@ -22,9 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Send HTML file
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
-});
+app.use(express.static(__dirname + "/public"));
 
 // Create - Tambah todolist
 app.post("/todolist", (req, res) => {
